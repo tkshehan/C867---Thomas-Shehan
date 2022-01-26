04ecc1caba238c0f7a7a8ecbdb7e0f2d880048e0
Thomas Shehan <thomas.shehan88@gmail.com>
2022-01-26 10:42:10 -0500

    convert to spaces

--- c867 - Thomas Shehan/student.cpp
+++ c867 - Thomas Shehan/student.cpp
@@ -3,68 +3,68 @@
 using namespace std;
 
 Student::Student(string id, string firstName, string lastName, string email, int age, int days1, int days2, int days3, DegreeProgram degree) {
-	this->studentId = id;
-	this->firstName = firstName;
-	this->lastName = lastName;
-	this->email = email;
-	this->age = age;
-	this->daysToComplete = vector<int>{ days1, days2, days3 };
-	this->degree = degree;
+    this->studentId = id;
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->email = email;
+    this->age = age;
+    this->daysToComplete = vector<int>{ days1, days2, days3 };
+    this->degree = degree;
 }
 
 void Student::setStudentId(string id) {
-	this->studentId = id;
+    this->studentId = id;
 }
 string Student::getStudentId() const {
-	return studentId;
+    return studentId;
 }
 
 void Student::setFirstName(string firstName) {
-	this->firstName = firstName;
+    this->firstName = firstName;
 }
 string Student::getFirstName() const {
-	return firstName;
+    return firstName;
 }
 
 void Student::setLastName(string lastName) {
-	this->lastName = lastName;
+    this->lastName = lastName;
 }
 string Student::getLastName() const {
-	return lastName;
+    return lastName;
 }
 
 void Student::setEmail(string email) {
-	this->email = email;
+    this->email = email;
 }
 string Student::getEmail() const {
-	return email;
+    return email;
 }
 
 void Student::setAge(int age) {
-	this->age = age;
+    this->age = age;
 }
 int Student::getAge() const {
-	return age;
+    return age;
 }
 
 void Student::setDaysToComplete(int days1, int days2, int days3) {
-	this->daysToComplete = vector<int>{ days1, days2, days3 };
+    this->daysToComplete = vector<int>{ days1, days2, days3 };
 }
 vector<int> Student::getDaysToComplete() const{
-	return daysToComplete;
+    return daysToComplete;
 }
 
 void Student::setDegree(DegreeProgram degree) {
-	this->degree = degree;
+    this->degree = degree;
 }
 DegreeProgram Student::getDegreeProgram() const {
-	return degree;
+    return degree;
 }
 
 void Student::print() {
-	string daysPrint = "{" + to_string(daysToComplete.at(0)) + "," + to_string(daysToComplete.at(1))+ "," + to_string(daysToComplete.at(2)) + "}";
-	cout << studentId << "\t First Name: " << firstName << "\t Last Name: " << lastName << "\t Age: " << age << "\t daysInCourse: "
-		<< daysPrint << " Degree Program: " << degree; // Degree may need conversion to string
-	// A1 [tab] First Name: John [tab] Last Name: Smith [tab] Age: 20 [tab]daysInCourse: {35, 40, 55} Degree Program: Security
+    string daysPrint = "{" + to_string(daysToComplete.at(0)) + "," + to_string(daysToComplete.at(1))+ ","+ to_string(daysToComplete.at(2)) + "}";
+    cout << studentId << "\t First Name: " << firstName << "\t Last Name: " << lastName << "\t Age: " << age << "\t daysInCourse: "
+        << daysPrint << " Degree Program: " << degree; // Degree may need conversion to string
+    // A1 [tab] First Name: John [tab] Last Name: Smith [tab] Age: 20 [tab]daysInCourse: {35, 40, 55} Degree Program: Security
 }
 
--- c867 - Thomas Shehan/student.h
+++ c867 - Thomas Shehan/student.h
@@ -8,39 +8,39 @@ using namespace std;
 
 class Student {
 public:
-	Student(string id, string firstName, string lastName, string email, int age, int days1, int days2, int days3, DegreeProgram degree);
+    Student(string id, string firstName, string lastName, string email, int age, int days1, int days2, int days3, DegreeProgram degree);
 
-	void setStudentId(string id);
-	string getStudentId() const;
+    void setStudentId(string id);
+    string getStudentId() const;
 
-	void setFirstName(string firstName);
-	string getFirstName() const;
+    void setFirstName(string firstName);
+    string getFirstName() const;
 
-	void setLastName(string lastName);
-	string getLastName() const;
+    void setLastName(string lastName);
+    string getLastName() const;
 
-	void setEmail(string email);
-	string getEmail() const;
+    void setEmail(string email);
+    string getEmail() const;
 
-	void setAge(int age);
-	int getAge() const;
+    void setAge(int age);
+    int getAge() const;
 
-	void setDaysToComplete(int days1, int days2, int days3);
-	vector<int> getDaysToComplete() const;
+    void setDaysToComplete(int days1, int days2, int days3);
+    vector<int> getDaysToComplete() const;
 
-	void setDegree(DegreeProgram degree);
-	DegreeProgram getDegreeProgram() const;
+    void setDegree(DegreeProgram degree);
+    DegreeProgram getDegreeProgram() const;
 
-	void print();
+    void print();
 
 private:
-	string studentId;
-	string firstName;
-	string lastName;
-	string email;
-	int age;
-	vector<int> daysToComplete;
-	DegreeProgram degree;
+    string studentId;
+    string firstName;
+    string lastName;
+    string email;
+    int age;
+    vector<int> daysToComplete;
+    DegreeProgram degree;
 };
 
 #endif
